d97f99fd413dd8c717afc90809776087850c1958
Mature Lion <maturelion1@gmail.com>
2026-04-02 20:29:13 +0100

    fix: remove use server to support static export

--- app/actions.ts
+++ app/actions.ts
@@ -1,4 +1,4 @@
-"use server";
+
 
 const getApiBaseUrl = () => process.env.API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api";
 const getApiKey = () => process.env.API_KEY || process.env.NEXT_PUBLIC_API_KEY || "";
